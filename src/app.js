3dc457a08903fef495d30498c6d773dad1331ecd
Afthabet <191522427+Afthabet@users.noreply.github.com>
2025-08-31 23:38:00 +0530

    bug:nginx

--- src/app.js
+++ src/app.js
@@ -7,13 +7,16 @@ const app = express();
 // --- Middleware ---
 app.use(cors({
   origin: [
-    "http://localhost:5173",         // local dev frontend
-    "http://afthabudheenet.me",      // production domain
-    "http://www.afthabudheenet.me"   // with www
+    "http://localhost:5173",        // for local dev
+    "http://afthabudheenet.me",     // your domain
+    "https://afthabudheenet.me",    // https domain
+    "http://www.afthabudheenet.me",
+    "https://www.afthabudheenet.me"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+;
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
