bfae4a7953709c3dd22374212fde6ff95061872b
Afthabet <191522427+Afthabet@users.noreply.github.com>
2025-08-31 12:14:45 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -6,11 +6,16 @@ const app = express();
 
 // --- Middleware ---
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",         // local dev frontend
+    "http://afthabudheenet.me",      // production domain
+    "http://www.afthabudheenet.me"   // with www
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-app.use(express.json({ limit: '50mb' })); 
+
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // ✅ Serve only uploads (public/uploads) at /uploads
