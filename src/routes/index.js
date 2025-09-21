456252711d18a0901bea1de666a4fe84df426aa1
Afthabet <191522427+Afthabet@users.noreply.github.com>
2025-09-21 12:19:07 +0530

    hotfix:activity log

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,10 @@ const userRoutes = require('./user.routes')
 const carRoutes = require('./car.routes');
 const authRoutes = require('./auth.routes'); 
 const activityLogRoutes = require('./activityLog.routes.js');
+
 router.use('/cars', carRoutes);
 router.use('/users', userRoutes);
 router.use('/auth', authRoutes); 
 router.use('/activity-log', activityLogRoutes);
+
 module.exports = router;
